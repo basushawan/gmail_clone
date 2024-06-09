d416f6374ee267811d25dabc88ff6cde294a6193
basushawan <shawanbasu.threes@gmail.com>
2024-06-09 18:59:57 +0530

    Build Navbar & Working on Body with routes*created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Navbar from './components/shared/Navbar'
+import Sidebar from './components/Sidebar'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Body from './components/Body'
+import Inbox from './components/Inbox'
+import Mail from './components/Mail'
+
+const router = createBrowserRouter([
+  {path:"/",
+    element:<Body/>,
+    children:[
+      {path:"/",
+        element:<Inbox/>
+      },
+      {path:"/mail/:id",
+        element:<Mail/>
+      },
+    ]
+  }
+])
 
 function App() {
   return (
-    <div>
+    <div className='bg-[#f6f8fc] h-screen w-screen overflow-hidden'>
       <Navbar/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
